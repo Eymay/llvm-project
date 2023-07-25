34823d434a0ea8a3c5149d1a0290cb3ef3e4cc42
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2023-07-25 12:56:35 +0300

    Simplified by replacing union with struct

--- llvm/lib/ExecutionEngine/JITLink/aarch32.cpp
+++ llvm/lib/ExecutionEngine/JITLink/aarch32.cpp
@@ -151,28 +151,22 @@ struct ThumbRelocation {
   const support::ulittle16_t &Lo; // Second halfword
 };
 
-union WritableArmRelocation {
-        WritableThumbRelocation Thumb; 
-        support::ulittle32_t Wd;      // Full word
+struct WritableArmRelocation { 
+  WritableArmRelocation(char *FixupPtr)
+      : Wd{*reinterpret_cast<support::ulittle32_t *>(FixupPtr)} {}
 
-  WritableArmRelocation(char *FixupPtr) : Thumb(FixupPtr) {}
-
+  support::ulittle32_t &Wd;
 };
 
 struct ArmRelocation {
-  /// Create a read-only reference to an ARM fixup.
+
   ArmRelocation(const char *FixupPtr)
-      : Wd{*reinterpret_cast<const support::ulittle32_t *>(FixupPtr)},
-        Hi{*reinterpret_cast<const support::ulittle16_t *>(FixupPtr)},
-        Lo{*reinterpret_cast<const support::ulittle16_t *>(FixupPtr + 2)}{}
+      : Wd{*reinterpret_cast<const support::ulittle32_t *>(FixupPtr)} {}
 
-  /// Create a read-only ARM fixup from a writeable one.
   ArmRelocation(WritableArmRelocation &Writable)
-      : Wd{Writable.Wd}, Hi{Writable.Thumb.Hi}, Lo(Writable.Thumb.Lo) {}
+      : Wd{Writable.Wd} {}
 
-  const support::ulittle32_t &Wd; // Full word
-  const support::ulittle16_t &Hi; // First halfword
-  const support::ulittle16_t &Lo; // Second halfword
+  const support::ulittle32_t &Wd;
 };
 
 Error makeUnexpectedOpcodeError(const LinkGraph &G, const ThumbRelocation &R,
